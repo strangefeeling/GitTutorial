888468a402efa2383a7554bb330d368275cb0079
rytis razmus <rytis@Rytiss-MacBook-Pro.local>
2017-11-24 13:15:49 +0200

    added some crap

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("This is a Git tutorial")
+        print("hey hoe")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
